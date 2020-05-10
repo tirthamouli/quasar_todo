fa4b5d79a011831d49afefd6d036a7e34d9f6651
tirthamoulib <btirthamouli@gmail.com>
2020-05-10 22:30:51 +0530

    List

---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+<li>
+  <div>{{ task.name }}</div>
+  <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+  <button @click="removeItem(task.id)">X</button>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    task: Object,
+    removeItem: Function,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,38 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <ul>
+      <task v-for="task in tasks" :key="task.id" :task="task" :removeItem="removeItem" />
+    </ul>
   </q-page>
 </template>
 
 <script>
+import Task from '../components/tasks/Tasks.vue';
+
 export default {
-  name: 'PageIndex',
+  name: 'Index',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1, name: 'Go to shop', dueDate: '2020/06/03', dueTime: '6:30',
+        },
+        {
+          id: 2, name: 'Get bananas', dueDate: '2020/06/04', dueTime: '7:30',
+        },
+        {
+          id: 3, name: 'Get apples', dueDate: '2020/06/05', dueTime: '8:30',
+        },
+      ],
+    };
+  },
+  methods: {
+    removeItem(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+  },
+  components: {
+    Task,
+  },
 };
 </script>
